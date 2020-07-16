4f1aee9bfc6a52a835bff9b6e49413a6a1b03035
Alec Thomas <alec@swapoff.org>
2020-07-16 16:17:17 +1000

    Circle + HIL docs.

--- hil/hil.go
+++ hil/hil.go
@@ -11,11 +11,16 @@ import (
 	hilast "github.com/hashicorp/hil/ast"
 )
 
+// Unmarshal HCL into v, interpolating the variables in "vars".
+//
+// Values in "vars" must be of type "int", "string", "map[string]interface{}"
+// or "[]interface{}".
 func Unmarshal(data []byte, v interface{}, vars map[string]interface{}) error {
 	hclAST, err := hcl.ParseBytes(data)
 	if err != nil {
 		return err
 	}
+	// Convert map into HIL variables.
 	hilVars := map[string]hilast.Variable{}
 	for key, value := range vars {
 		switch value := value.(type) {
@@ -23,6 +28,12 @@ func Unmarshal(data []byte, v interface{}, vars map[string]interface{}) error {
 			hilVars[key] = hilast.Variable{
 				Value: value,
 				Type:  hilast.TypeString,
+			}
+
+		case []interface{}:
+			hilVars[key] = hilast.Variable{
+				Value: value,
+				Type:  hilast.TypeList,
 			}
 
 		case map[string]interface{}:
@@ -41,8 +52,19 @@ func Unmarshal(data []byte, v interface{}, vars map[string]interface{}) error {
 			return fmt.Errorf("unsupported variable type %T for %q", value, key)
 		}
 	}
+	err = InterpolateHIL(hclAST, &hil.EvalConfig{
+		GlobalScope: &hilast.BasicScope{VarMap: hilVars},
+	})
+	if err != nil {
+		return err
+	}
+	return hcl.UnmarshalAST(hclAST, v)
+}
+
+// InterpolateHIL interpolates all string values in "node" using the given hil.EvalConfig.
+func InterpolateHIL(node hcl.Node, config *hil.EvalConfig) error {
 	// Interpolate into AST.
-	err = hcl.Visit(hclAST, func(node hcl.Node, next func() error) error {
+	return hcl.Visit(node, func(node hcl.Node, next func() error) error {
 		value, ok := node.(*hcl.Value)
 		if !ok || value.Str == nil {
 			return next()
@@ -52,9 +74,7 @@ func Unmarshal(data []byte, v interface{}, vars map[string]interface{}) error {
 		if err != nil {
 			return participle.Errorf(value.Pos, "%s", err.Error())
 		}
-		out, err := hil.Eval(hilNode, &hil.EvalConfig{
-			GlobalScope: &hilast.BasicScope{VarMap: hilVars},
-		})
+		out, err := hil.Eval(hilNode, config)
 		if err != nil {
 			return participle.Errorf(value.Pos, "%s", err.Error())
 		}
@@ -62,8 +82,4 @@ func Unmarshal(data []byte, v interface{}, vars map[string]interface{}) error {
 		value.Str = &str
 		return next()
 	})
-	if err != nil {
-		return err
-	}
-	return hcl.UnmarshalAST(hclAST, v)
 }
